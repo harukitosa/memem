21ef309e6f4b61db202714278950d310c355d857
harukitosa <13haruki28@gmail.com>
2022-07-14 23:17:31 +0900

    feat(memem) callback関数の追加

--- memem.go
+++ memem.go
@@ -1,6 +1,8 @@
 package memem
 
-import "sync"
+import (
+	"sync"
+)
 
 type Cache interface {
 	Set(key string, value interface{})
@@ -8,8 +10,17 @@ type Cache interface {
 }
 
 type CacheInMemory struct {
-	store map[string]interface{}
+	store    map[string]interface{}
+	callback func() interface{}
 	sync.Mutex
+}
+
+func NewCacheWithCallback(callback func() interface{}) Cache {
+	m := make(map[string]interface{})
+	return &CacheInMemory{
+		store:    m,
+		callback: callback,
+	}
 }
 
 func NewCache() Cache {
@@ -26,5 +37,12 @@ func (c *CacheInMemory) Set(key string, value interface{}) {
 }
 
 func (c *CacheInMemory) Get(key string) interface{} {
+	value := c.store[key]
+	// 値が存在しなくてcallbackがある場合はそれを利用する
+	if value == nil && c.callback != nil {
+		callbackValue := c.callback()
+		c.Set(key, callbackValue)
+		return callbackValue
+	}
 	return c.store[key]
 }
--- memem_test.go
+++ memem_test.go
@@ -1,6 +1,8 @@
 package memem
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCache(t *testing.T) {
 	c := NewCache()
@@ -11,5 +13,41 @@ func TestCache(t *testing.T) {
 	c.Set("hoge", "doremifaso")
 	if c.Get("hoge") != "doremifaso" {
 		t.Error("error")
+	}
+}
+
+func TestArrayCache(t *testing.T) {
+	c := NewCache()
+	// var arr[2] string = [2]string {"Golang", "Java"}
+	slice := []string{"Golang", "Java"}
+	c.Set("key", slice)
+	value := c.Get("key")
+	v, ok := value.([]string)
+	if !ok {
+		t.Error("error cast")
+	}
+	if v[0] != "Golang" {
+		t.Error("error not match")
+	}
+}
+
+func TestGetDataIsNoneCache(t *testing.T) {
+	c := NewCache()
+	value := c.Get("key")
+	if value != nil {
+		t.Error("not nil")
+	}
+}
+
+func TestCallbackCache(t *testing.T) {
+	c := NewCacheWithCallback(func() interface{} {
+		return "callback result"
+	})
+	value := c.Get("callback result")
+	if value == nil {
+		t.Error("is not callback value")
+	}
+	if value != "callback result" {
+		t.Error("is not callback value")
 	}
 }
